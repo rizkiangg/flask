908f3ed001067b02f06652c36281aa48832a380c
rizkiangg <rizkiagung629@gmaill.com.com>
2024-10-15 07:43:37 +0700

    First commit

--- config/db_config.py
+++ config/db_config.py
@@ -3,9 +3,9 @@ from flask import Flask
 
 def init_db(app: Flask):
     app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'agung'
-    app.config['MYSQL_PASSWORD'] = 'Da240219'
-    app.config['MYSQL_DB'] = 'my_db'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_DB'] = 'users'
 
     mysql = MySQL(app)
     return mysql
